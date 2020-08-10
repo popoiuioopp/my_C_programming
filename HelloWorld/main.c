2fd9be4a86335944da84a858826f692683805ed1
PongpichS <biggerbosssuper@gmail.com>
2020-08-10 14:36:16 +0700

    Add comparing to lucky's

--- HelloWorld/main.c
+++ HelloWorld/main.c
@@ -4,11 +4,31 @@
 
 int main()
 {
+    /*
     int number;
     printf("Put in the number : ");
     scanf("%d", &number);
     isPrime(number);
     return 0;
+    int count =0;
+
+
+    int i = 1;
+    while (i < 10000){
+        compare(i);
+        if(compare(i) == 0)
+        {
+            count++;
+        }
+        i++;
+
+    printf("%d", count);*/
+    int i = 100000000;
+    while (i < 100001000)
+    {
+        isPrime(i);
+        i++;
+    }
 }
 
 int isPrime(int number)
@@ -16,7 +36,7 @@ int isPrime(int number)
     double temp = sqrt((double)number);
     if (number == 1)
     {
-        printf("NOT PRIME");
+        //printf("%d IS NOT PRIME\n", number);
         return 0;
     }
 
@@ -24,10 +44,54 @@ int isPrime(int number)
     {
         if ((int) number % i == 0)
         {
-            printf("NOT PRIME");
+            //printf("%d IS NOT PRIME\n", number);
             return 0;
         }
     }
-    printf("IS PRIME");
+    printf("%d IS PRIME\n", number);
+    return 1;
+}
+
+int isPrimeL(int primeNum)
+{
+    //printf("%d",primeNum);
+    const int length = 4;
+    int checkList[] = {2,3,5,7};
+    int temp;
+    int result;
+    if (primeNum == 1){
+        //printf("phor mueng die");
+        return 0;
+    }
+    for(int i = 0; i < length; i++){
+        temp = checkList[i];
+        if (primeNum % temp  == 0){
+            if (primeNum != temp){
+                result = 1;
+            break;
+            }
+        }
+        else{
+            result = 0;
+        }
+    }
+    if (result == 1){
+        //printf("not prime desu\n");
+        return 0;
+    }
+    else{
+        //printf("prime desu\n");
+        return 1;
+    }
+    return 0;
+}
+
+int compare(int number)
+{
+    if (isPrime(number) != isPrimeL(number))
+    {
+        printf("%d is not the same!\nBoss=%d and Lucky=%d\n", number, isPrime(number), isPrimeL(number));
+        return 0;
+    }
     return 1;
 }
